dc80b1886485acc85239e17b76073623878f33b9
Pedro dos Santos Borges <56660097+peuborg@users.noreply.github.com>
2022-09-01 20:55:59 -0300

    Aula05

---
+++ Aula05/pessoajuridica.py
@@ -0,0 +1,20 @@
+from pessoa import Pessoa
+
+class PessoaJuridica(Pessoa):
+    #Construtor da classe
+    def __init__(self, cnpj, nome, idade) -> None:
+        super().__init__(nome,idade) #Chamada do construtor da classe Pessoa
+        self.cnpj = cnpj
+
+    def setCNPJ(self, cnpj):
+        self.cnpj = cnpj
+
+    def getCNPJ(self):
+        return self.cnpj
+
+    def imprimePessoaJuridica(self):
+        print('Olá ' + self.nome + '. Seja bem vindo ao curso de Python.')
+        print('---------------------------')
+        print('Nome: ' + self.nome)
+        print('Idade: ' + self.idade)
+        print('CNPJ: ' + self.cnpj)
