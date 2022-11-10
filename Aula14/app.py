09b78f33f6f0f65654271a1ce299b7ce8456efd4
peuborg <peuborg.8@gmail.com>
2022-11-10 19:05:46 -0300

    aula 14

--- Aula14/app.py
+++ Aula14/app.py
@@ -8,42 +8,52 @@ class Application:
         self.container1 = Frame(master)
         self.container1["pady"] = 10
         self.container1.pack()
+
         self.container2 = Frame(master)
         self.container2["padx"] = 20
         self.container2["pady"] = 5
         self.container2.pack()
+
         self.container3 = Frame(master)
         self.container3["padx"] = 20
         self.container3["pady"] = 5
         self.container3.pack()
+
         self.container4 = Frame(master)
         self.container4["padx"] = 20
         self.container4["pady"] = 5
         self.container4.pack()
+
         self.container5 = Frame(master)
         self.container5["padx"] = 20
         self.container5["pady"] = 5
         self.container5.pack()
+
         self.container6 = Frame(master)
         self.container6["padx"] = 20
         self.container6["pady"] = 5
         self.container6.pack()
+
         self.container7 = Frame(master)
         self.container7["padx"] = 20
         self.container7["pady"] = 5
         self.container7.pack()
+
         self.container8 = Frame(master)
         self.container8["padx"] = 20
         self.container8["pady"] = 10
         self.container8.pack()
+
         self.container9 = Frame(master)
         self.container9["pady"] = 15
         self.container9.pack()
 
+        print("Criação de Label no 1º Container")
         self.titulo = Label(self.container1, text="Informe os dados :")
         self.titulo["font"] = ("Calibri", "9", "bold")
         self.titulo.pack ()
 
+        print("Criação de Label, Entry e Button no 2º Container")
         self.lblidusuario = Label(self.container2, text="idUsuario:", font=self.fonte, width=10)
         self.lblidusuario.pack(side=LEFT)
 
@@ -56,6 +66,7 @@ class Application:
         self.btnBuscar["command"] = self.buscarUsuario
         self.btnBuscar.pack(side=RIGHT)
 
+        print("Criação de Label e Entry no 3º Container - NOME")
         self.lblnome = Label(self.container3, text="Nome:", font=self.fonte, width=10)
         self.lblnome.pack(side=LEFT)
 
@@ -64,6 +75,7 @@ class Application:
         self.txtnome["font"] = self.fonte
         self.txtnome.pack(side=LEFT)
 
+        print("Criação de Label e Entry no 4º Container - TELEFONE")
         self.lbltelefone = Label(self.container4, text="Telefone:", font=self.fonte, width=10)
         self.lbltelefone.pack(side=LEFT)
 
@@ -72,6 +84,7 @@ class Application:
         self.txttelefone["font"] = self.fonte
         self.txttelefone.pack(side=LEFT)
 
+        print("Criação de Label e Entry no 5º Container - EMAIL")
         self.lblemail= Label(self.container5, text="E-mail:", font=self.fonte, width=10)
         self.lblemail.pack(side=LEFT)
 
@@ -80,6 +93,7 @@ class Application:
         self.txtemail["font"] = self.fonte
         self.txtemail.pack(side=LEFT)
 
+        print("Criação de Label e Entry no 6º Container - USUÁRIO")
         self.lblusuario= Label(self.container6, text="Usuário:", font=self.fonte, width=10)
         self.lblusuario.pack(side=LEFT)
 
@@ -88,6 +102,7 @@ class Application:
         self.txtusuario["font"] = self.fonte
         self.txtusuario.pack(side=LEFT)
 
+        print("Criação de Label e Entry no 7º Container - SENHA")
         self.lblsenha= Label(self.container7, text="Senha:", font=self.fonte, width=10)
         self.lblsenha.pack(side=LEFT)
 
@@ -97,6 +112,7 @@ class Application:
         self.txtsenha["font"] = self.fonte
         self.txtsenha.pack(side=LEFT)
 
+        print("Criação de Buttons no 8º Container")
         self.bntInsert = Button(self.container8, text="Inserir", font=self.fonte, width=12)
         self.bntInsert["command"] = self.inserirUsuario
         self.bntInsert.pack (side=LEFT)
@@ -109,10 +125,18 @@ class Application:
         self.bntExcluir["command"] = self.excluirUsuario
         self.bntExcluir.pack(side=LEFT)
 
+        print("Criação de Label no 9º Container - MENSAGEM")
         self.lblmsg = Label(self.container9, text="")
         self.lblmsg["font"] = ("Verdana", "9", "italic")
         self.lblmsg.pack()
 
+    def limparCampos(self):
+            self.txtidusuario.delete(0, END)
+            self.txtnome.delete(0, END)
+            self.txttelefone.delete(0, END)
+            self.txtemail.delete(0, END)
+            self.txtusuario.delete(0, END)
+            self.txtsenha.delete(0, END)
 
     def inserirUsuario(self):
         user = Usuarios()
@@ -124,15 +148,9 @@ class Application:
         user.senha = self.txtsenha.get()
 
         self.lblmsg["text"] = user.insertUser()
+        print(self.lblmsg["text"])
 
-        self.txtidusuario.delete(0, END)
-        self.txtnome.delete(0, END)
-        self.txttelefone.delete(0, END)
-        self.txtemail.delete(0, END)
-        self.txtusuario.delete(0, END)
-        self.txtsenha.delete(0, END)
-
-
+        self.limparCampos
 
     def alterarUsuario(self):
         user = Usuarios()
@@ -145,15 +163,9 @@ class Application:
         user.senha = self.txtsenha.get()
 
         self.lblmsg["text"] = user.updateUser()
+        print(self.lblmsg["text"])
 
-        self.txtidusuario.delete(0, END)
-        self.txtnome.delete(0, END)
-        self.txttelefone.delete(0, END)
-        self.txtemail.delete(0, END)
-        self.txtusuario.delete(0, END)
-        self.txtsenha.delete(0, END)
-
-
+        self.limparCampos
 
     def excluirUsuario(self):
         user = Usuarios()
@@ -161,14 +173,9 @@ class Application:
         user.idusuario = self.txtidusuario.get()
 
         self.lblmsg["text"] = user.deleteUser()
+        print(self.lblmsg["text"])
 
-        self.txtidusuario.delete(0, END)
-        self.txtnome.delete(0, END)
-        self.txttelefone.delete(0, END)
-        self.txtemail.delete(0, END)
-        self.txtusuario.delete(0, END)
-        self.txtsenha.delete(0, END)
-
+        self.limparCampos
 
     def buscarUsuario(self):
         user = Usuarios()
@@ -176,6 +183,7 @@ class Application:
         idusuario = self.txtidusuario.get()
 
         self.lblmsg["text"] = user.selectUser(idusuario)
+        print(self.lblmsg["text"])
 
         self.txtidusuario.delete(0, END)
         self.txtidusuario.insert(INSERT, user.idusuario)
@@ -195,7 +203,7 @@ class Application:
         self.txtsenha.delete(0, END)
         self.txtsenha.insert(INSERT,user.senha)
 
-print("####Início do programa####")
+print("####Início do programa - App de Usuários####")
 root = Tk()
 Application(root)
 root.mainloop()
