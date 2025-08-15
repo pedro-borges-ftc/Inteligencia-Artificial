3040bf3491d878fcc4a99fe0396f28d459fe0d99
peuborg <peuborg.8@gmail.com>
2025-08-15 11:46:16 -0300

    antes aula 03

--- Aula01/exemplo.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_iris
-import pandas as pd
-
-# Carrega o dataset
-iris = load_iris()
-df = pd.DataFrame(iris.data, columns=iris.feature_names)
-
-# Mostra estatísticas básicas
-print(df.describe())
